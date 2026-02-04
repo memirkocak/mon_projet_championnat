a403a4790cce54928044d79195b76f971275f2cf
memirkocak <mekocak@edenschool.fr>
2026-02-04 09:32:58 +0100

    feat(app): add country create page

--- src/Controller/CountryController.php
+++ src/Controller/CountryController.php
@@ -2,16 +2,61 @@
 
 namespace App\Controller;
 
+use App\Entity\Country;
+use App\Form\CountryFormType;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\File\Exception\FileException;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
+use Symfony\Component\String\Slugger\SluggerInterface;
 
 class CountryController extends AbstractController
 {
     #[Route('/country/create', name: 'app_country_create')]
-    public function create(): Response
-    {
-        return $this->render('country/create.html.twig');
+    public function create(
+        Request $request,
+        EntityManagerInterface $entityManager,
+        SluggerInterface $slugger
+    ): Response {
+        $country = new Country();
+        $form = $this->createForm(CountryFormType::class, $country);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $logoFile = $form->get('logo')->getData();
+
+            if ($logoFile) {
+                $originalFilename = pathinfo($logoFile->getClientOriginalName(), PATHINFO_FILENAME);
+                $safeFilename = $slugger->slug($originalFilename);
+                $newFilename = $safeFilename . '-' . uniqid() . '.' . $logoFile->guessExtension();
+
+                try {
+                    $logoFile->move(
+                        $this->getParameter('logos_directory'),
+                        $newFilename
+                    );
+                } catch (FileException $e) {
+                    $this->addFlash('error', 'Erreur lors de l\'upload du logo.');
+                    return $this->render('country/create.html.twig', [
+                        'countryForm' => $form,
+                    ]);
+                }
+
+                $country->setLogo($newFilename);
+            }
+
+            $entityManager->persist($country);
+            $entityManager->flush();
+
+            $this->addFlash('success', 'Le pays a été créé avec succès !');
+            return $this->redirectToRoute('app_home');
+        }
+
+        return $this->render('country/create.html.twig', [
+            'countryForm' => $form,
+        ]);
     }
 }
 
---
+++ src/Form/CountryFormType.php
@@ -0,0 +1,62 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Country;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\FileType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Validator\Constraints\File;
+use Symfony\Component\Validator\Constraints\NotBlank;
+
+class CountryFormType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+            ->add('name', TextType::class, [
+                'label' => 'Nom du pays',
+                'attr' => [
+                    'class' => 'form-control',
+                    'placeholder' => 'Ex: France'
+                ],
+                'constraints' => [
+                    new NotBlank([
+                        'message' => 'Veuillez entrer un nom de pays',
+                    ]),
+                ],
+            ])
+            ->add('logo', FileType::class, [
+                'label' => 'Logo du pays',
+                'required' => false,
+                'mapped' => false,
+                'attr' => [
+                    'class' => 'form-control',
+                    'accept' => 'image/*'
+                ],
+                'constraints' => [
+                    new File([
+                        'maxSize' => '2M',
+                        'mimeTypes' => [
+                            'image/jpeg',
+                            'image/png',
+                            'image/gif',
+                            'image/webp',
+                        ],
+                        'mimeTypesMessage' => 'Veuillez uploader une image valide (JPEG, PNG, GIF ou WebP)',
+                    ])
+                ],
+            ])
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            'data_class' => Country::class,
+        ]);
+    }
+}
+
